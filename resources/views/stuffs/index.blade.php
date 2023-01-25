4e92a9417a7a9f6ec35d3433586a8d7370c9f5bc
ErfanAfshari <erfanafsharii@gmail.com>
2023-01-25 23:50:33 +0330

    تغییرات ظاهری

--- resources/views/stuffs/index.blade.php
+++ resources/views/stuffs/index.blade.php
@@ -1,16 +1,24 @@
 <!DOCTYPE html>
-<!--=== Coding by CodingLab | www.codinglabweb.com === -->
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <!----======== CSS ======== -->
     <link rel="stylesheet" href="style.css">
      
     <!----===== Iconscout CSS ===== -->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+
+    <script
+        src="https://code.jquery.com/jquery-3.6.3.min.js"
+        integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU="
+        crossorigin="anonymous">
+    </script>
 
     <title>Admin Dashboard Panel</title> 
 </head>
@@ -21,7 +29,7 @@
                 <img src="images/logo.png" alt="">
             </div>
 
-            <span class="logo_name">CodingLab</span>
+            <span class="logo_name">Web Project</span>
         </div>
 
         <div class="menu-items">
@@ -87,92 +95,73 @@
         <div class="dash-content">
             <div class="overview">
                 <div class="title">
-                    <i class="uil uil-tachometer-fast-alt"></i>
-                    <span class="text">Dashboard</span>
+                <i class="material-icons">list</i>
+                <span class="text">Products List</span>
                 </div>
-          
+            </div>
+        </div>
                 
-        @yield('maincontent')           
+    
+    <div class="col-md-11" style="margin:auto;margin-top:12px;">
         
-        <div class="boxes">
-                    <div class="box box1">
-                        <i class="uil uil-thumbs-up"></i>
-                        <span class="text">Total Likes</span>
-                        <span class="number">50,120</span>
-                    </div>
-                    <div class="box box2">
-                        <i class="uil uil-comments"></i>
-                        <span class="text">Comments</span>
-                        <span class="number">20,120</span>
-                    </div>
-                    <div class="box box3">
-                        <i class="uil uil-share"></i>
-                        <span class="text">Total Share</span>
-                        <span class="number">10,120</span>
-                    </div>
-                </div>
-            </div>
-
-            <div class="activity">
+        <table class="table table-bordered" style="marfin-top:20px;">
+            <thead>
+                <th>id</th>
+                <th>title</th>
+                <th>price</th>
+                <th>image</th>
+                <th>edit</th>
+                <th>delete item</th>
+            </thead>
+            <tbody>
+                <tbody>
+                    @foreach ($stuffs as $stuff)
+                        <tr>
+                            <td> {{ $stuff->id}}     </td>
+                            <td> {{ $stuff->title}}  </td>
+                            <td> {{ $stuff->price}}  </td>
+                            <td> {{ $stuff->image}}  </td>
+                            <td>
+                            <div style="margin:auto"><a href="{{route('updateproduct',['id'=>$stuff->id])}}" target="_blank" class="btn btn-outline-primary" > Update </a></div>
+                            <div style="margin:auto;margin-top:5px;"><a href="{{route('showproduct',['id'=>$stuff->id])}}" target="_blank" class="btn btn-outline-dark" > Show </a></div>
+                            </td>
+                            <td>
+                            <form action="{{route('deleteproduct',['id'=>$stuff->id])}}" method="post">
+                                    @csrf
+                                    <input type="hidden" name="_method" value="delete">
+                                    <input type="submit" value="Delete" class="btn btn-outline-danger deletesubmit">   
+                                </form> 
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>  
+            </tbody>       
+        </table>
+    
+    </div>
+    <div class="dash-content">
+            <div class="overview">
                 <div class="title">
-                    <i class="uil uil-clock-three"></i>
-                    <span class="text">Recent Activity</span>
-                </div>
-
-                <div class="activity-data">
-                    <div class="data names">
-                        <span class="data-title">Name</span>
-                        <span class="data-list">Prem Shahi</span>
-                        <span class="data-list">Deepa Chand</span>
-                        <span class="data-list">Manisha Chand</span>
-                        <span class="data-list">Pratima Shahi</span>
-                        <span class="data-list">Man Shahi</span>
-                        <span class="data-list">Ganesh Chand</span>
-                        <span class="data-list">Bikash Chand</span>
-                    </div>
-                    <div class="data email">
-                        <span class="data-title">Email</span>
-                        <span class="data-list">premshahi@gmail.com</span>
-                        <span class="data-list">deepachand@gmail.com</span>
-                        <span class="data-list">prakashhai@gmail.com</span>
-                        <span class="data-list">manishachand@gmail.com</span>
-                        <span class="data-list">pratimashhai@gmail.com</span>
-                        <span class="data-list">manshahi@gmail.com</span>
-                        <span class="data-list">ganeshchand@gmail.com</span>
-                    </div>
-                    <div class="data joined">
-                        <span class="data-title">Joined</span>
-                        <span class="data-list">2022-02-12</span>
-                        <span class="data-list">2022-02-12</span>
-                        <span class="data-list">2022-02-13</span>
-                        <span class="data-list">2022-02-13</span>
-                        <span class="data-list">2022-02-14</span>
-                        <span class="data-list">2022-02-14</span>
-                        <span class="data-list">2022-02-15</span>
-                    </div>
-                    <div class="data type">
-                        <span class="data-title">Type</span>
-                        <span class="data-list">New</span>
-                        <span class="data-list">Member</span>
-                        <span class="data-list">Member</span>
-                        <span class="data-list">New</span>
-                        <span class="data-list">Member</span>
-                        <span class="data-list">New</span>
-                        <span class="data-list">Member</span>
-                    </div>
-                    <div class="data status">
-                        <span class="data-title">Status</span>
-                        <span class="data-list">Liked</span>
-                        <span class="data-list">Liked</span>
-                        <span class="data-list">Liked</span>
-                        <span class="data-list">Liked</span>
-                        <span class="data-list">Liked</span>
-                        <span class="data-list">Liked</span>
-                        <span class="data-list">Liked</span>
-                    </div>
+                <i class="material-icons">add</i>
+                    <span class="text">    
+                        <a href="{{route('insertproduct')}}"class="btn btn-secondary btn-lg btn-block" > Add Product </a>
+                    </span>
                 </div>
             </div>
-        </div>       
+        </div>
+   
+    <script>
+        $(document).ready(function(){
+            $('.deletesubmit').on('click',function(e){
+                e.preventDefault();
+                if(confirm('Do you want to delete this item?'))
+                {
+                    var form = $(this).closest('form');
+                    form.submit();
+                }
+            })
+        })
+    </script>         
 
             
     </section>
--- resources/views/stuffs/stuffs.balde.php
+++ resources/views/stuffs/stuffs.balde.php
@@ -1,63 +1,29 @@
 @extends('stuffs.index')
 @section('maincontent')
-<di calss="row">
-<body>
-    <div class="col-md-9" style="margin:auto;margin-top:20px;">
+
+<div class="dash-content">
+            <div class="overview">
+                <div class="title">
+                    <i class="uil uil-tachometer-fast-alt"></i>
+                    <span class="text">Dashboard</span>
+                </div>
         
-    
-        <h1>Products List</h1>
-
-        <table class="table table-bordered" style="marfin-top:20px;">
-            <thead>
-                <th>id</th>
-                <th>title</th>
-                <th>price</th>
-                <th>image</th>
-                <th>edit</th>
-                <th>delete item</th>
-            </thead>
-            <tbody>
-                <tbody>
-                    @foreach ($stuffs as $stuff)
-                        <tr>
-                            <td> {{ $stuff->id}}     </td>
-                            <td> {{ $stuff->title}}  </td>
-                            <td> {{ $stuff->price}}  </td>
-                            <td> {{ $stuff->image}}  </td>
-                            <td>
-                            <div style="margin:auto"><a href="{{route('updateproduct',['id'=>$stuff->id])}}" target="_blank" class="btn btn-outline-primary" > Update </a></div>
-                            <div style="margin:auto;margin-top:5px;"><a href="{{route('showproduct',['id'=>$stuff->id])}}" target="_blank" class="btn btn-outline-dark" > Show </a></div>
-                            </td>
-                            <td>
-                            <form action="{{route('deleteproduct',['id'=>$stuff->id])}}" method="post">
-                                    @csrf
-                                    <input type="hidden" name="_method" value="delete">
-                                    <input type="submit" value="Delete" class="btn btn-outline-danger deletesubmit">   
-                                </form> 
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>  
-            </tbody>        
-        </table>
-    
-    </div>
-    
-    <div class="col-md-9" style="margin:auto;">
-    <a href="{{route('insertproduct')}}"class="btn btn-secondary btn-lg btn-block" > Add Product </a>
-    </div>
-
-</div>
-<script>
-        $(document).ready(function(){
-            $('.deletesubmit').on('click',function(e){
-                e.preventDefault();
-                if(confirm('Do you want to delete this item?'))
-                {
-                    var form = $(this).closest('form');
-                    form.submit();
-                }
-            })
-        })
-    </script>
+                <div class="boxes">
+                    <div class="box box1">
+                        <i class="uil uil-thumbs-up"></i>
+                        <span class="text">Total Likes</span>
+                        <span class="number">50,120</span>
+                    </div>
+                    <div class="box box2">
+                        <i class="uil uil-comments"></i>
+                        <span class="text">Comments</span>
+                        <span class="number">20,120</span>
+                    </div>
+                    <div class="box box3">
+                        <i class="uil uil-share"></i>
+                        <span class="text">Total Share</span>
+                        <span class="number">10,120</span>
+                    </div>
+                </div>
+            </div>
 @endsection
